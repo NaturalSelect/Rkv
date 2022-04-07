d15eb5b69c6cedb5038cd0d4fe45ff997fe7847f
NaturalSharp <2145973003@qq.com>
2022-04-08 02:11:42 +0800

    add code

--- Client/src/App.cpp
+++ Client/src/App.cpp
@@ -1,13 +1,27 @@
 #include <cstdio>
 #include <sharpen/EventEngine.hpp>
 #include <sharpen/INetStreamChannel.hpp>
+#include <sharpen/IpEndPoint.hpp>
+#include <sharpen/Converter.hpp>
+#include <rkv/PutRequest.hpp>
+#include <rkv/PutResponse.hpp>
+#include <rkv/GetRequest.hpp>
+#include <rkv/GetResponse.hpp>
+#include <rkv/DeleteRequest.hpp>
+#include <rkv/DeleteResponse.hpp>
+#include <rkv/LeaderRedirectResponse.hpp>
+#include <rkv/MessageHeader.hpp>
 
-void Entry()
+static void ConnectTo(const char *ip,std::uint16_t port)
+{
+
+}
+
+static void Entry()
 {
     sharpen::StartupNetSupport();
     sharpen::CleanupNetSupport();
 }
-
 
 int main(int argc, char const *argv[])
 {
