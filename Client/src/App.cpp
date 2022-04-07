71f24c4831da60c94210c9b9971bd751ccd18722
NaturalSharp <2145973003@qq.com>
2022-04-08 02:40:10 +0800

    add

--- Client/src/App.cpp
+++ Client/src/App.cpp
@@ -12,9 +12,55 @@
 #include <rkv/LeaderRedirectResponse.hpp>
 #include <rkv/MessageHeader.hpp>
 
-static void ConnectTo(const char *ip,std::uint16_t port)
+static sharpen::NetStreamChannelPtr ConnectTo(const char *ip,std::uint16_t port)
 {
+    sharpen::IpEndPoint ep{0,0};
+    sharpen::NetStreamChannelPtr channel = sharpen::MakeTcpStreamChannel(sharpen::AddressFamily::Ip);
+    channel->Bind(ep);
+    channel->Register(sharpen::EventEngine::GetEngine());
+    ep.SetAddrByString(ip);
+    ep.SetPort(port);
+    channel->ConnectAsync(ep);
+    return channel;
+}
 
+static void WriteMessage(sharpen::NetStreamChannelPtr channel,const rkv::MessageHeader &header)
+{
+    std::size_t sz{channel->WriteObjectAsync(header)};
+    if(sz != sizeof(header))
+    {
+        sharpen::ThrowSystemError(sharpen::ErrorConnectReset);
+    }
+}
+
+static void WriteMessage(sharpen::NetStreamChannelPtr channel,const rkv::MessageHeader &header,const sharpen::ByteBuffer &request)
+{
+    WriteMessage(channel,header);
+    std::size_t sz{channel->WriteAsync(request)};
+    if(sz != request.GetSize())
+    {
+        sharpen::ThrowSystemError(sharpen::ErrorConnectReset);
+    }
+}
+
+static void ReadMessage(sharpen::NetStreamChannelPtr channel,rkv::MessageType expectedType,sharpen::ByteBuffer &response)
+{
+    rkv::MessageHeader header;
+    std::size_t sz{channel->ReadObjectAsync(header)};
+    if(sz != sizeof(header))
+    {
+        sharpen::ThrowSystemError(sharpen::ErrorConnectReset);
+    }
+    if(rkv::GetMessageType(header) != expectedType)
+    {
+        throw std::logic_error("got unexpected response");
+    }
+    response.ExtendTo(sharpen::IntCast<std::size_t>(header.size_));
+    sz = channel->ReadFixedAsync(response);
+    if(sz != response.GetSize())
+    {
+        sharpen::ThrowSystemError(sharpen::ErrorConnectReset);
+    }
 }
 
 static void Entry()
