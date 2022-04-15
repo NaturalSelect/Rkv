4e7b39b1ee8c2701f27cda43d4d1a10db7999b6b
NaturalSharp <2145973003@qq.com>
2022-04-16 01:03:27 +0800

    update codes

--- Server/include/rkv/CompletedMigration.hpp
+++ Server/include/rkv/CompletedMigration.hpp
@@ -5,25 +5,27 @@
 #include <utility>
 
 #include <sharpen/ByteBuffer.hpp>
+#include <sharpen/BinarySerializable.hpp>
 
 namespace rkv
 {
-    class CompletedMigration
+    class CompletedMigration:public sharpen::BinarySerializable<rkv::CompletedMigration>
     {
     private:
         using Self = rkv::CompletedMigration;
     
+        std::uint64_t id_;
         std::uint64_t source_;
         std::uint64_t destination_;
         sharpen::ByteBuffer beginKey_;
         sharpen::ByteBuffer endKey_;
     public:
     
-        CompletedMigration();
+        CompletedMigration() = default;
     
-        CompletedMigration(const Self &other);
+        CompletedMigration(const Self &other) = default;
     
-        CompletedMigration(Self &&other) noexcept;
+        CompletedMigration(Self &&other) noexcept = default;
     
         inline Self &operator=(const Self &other)
         {
@@ -40,6 +42,62 @@ namespace rkv
         {
             return *this;
         }
+
+        inline std::uint64_t GetId() const noexcept
+        {
+            return this->id_;
+        }
+
+        inline void SetId(std::uint64_t id) noexcept
+        {
+            this->id_ = id;
+        }
+
+        inline std::uint64_t GetSource() const noexcept
+        {
+            return this->source_;
+        }
+
+        inline void SetSource(std::uint64_t source) noexcept
+        {
+            this->source_ = source;
+        }
+
+        inline std::uint64_t GetDestination() const noexcept
+        {
+            return this->destination_;
+        }
+
+        inline void SetDestination(std::uint64_t destination) noexcept
+        {
+            this->destination_ = destination;
+        }
+
+        inline sharpen::ByteBuffer &BeginKey() noexcept
+        {
+            return this->beginKey_;
+        }
+
+        inline const sharpen::ByteBuffer &BeginKey() const noexcept
+        {
+            return this->beginKey_;
+        }
+
+        inline sharpen::ByteBuffer &EndKey() noexcept
+        {
+            return this->endKey_;
+        }
+
+        inline const sharpen::ByteBuffer &EndKey() const noexcept
+        {
+            return this->endKey_;
+        }
+
+        std::size_t ComputeSize() const noexcept;
+
+        std::size_t LoadFrom(const char *data,std::size_t size);
+
+        std::size_t UnsafeStoreTo(char *data) const noexcept;
     };   
 }
 
--- Server/src/CompletedMigration.cpp
+++ Server/src/CompletedMigration.cpp
@@ -1,1 +1,64 @@
 #include <rkv/CompletedMigration.hpp>
+
+std::size_t rkv::CompletedMigration::ComputeSize() const noexcept
+{
+    std::size_t size{0};
+    sharpen::Varuint64 builder{this->id_};
+    size += Helper::ComputeSize(builder);
+    builder.Set(this->source_);
+    size += Helper::ComputeSize(builder);
+    builder.Set(this->destination_);
+    size += Helper::ComputeSize(builder);
+    size += Helper::ComputeSize(this->beginKey_);
+    size += Helper::ComputeSize(this->endKey_);
+    return size;
+}
+
+std::size_t rkv::CompletedMigration::LoadFrom(const char *data,std::size_t size)
+{
+    if(size < 5)
+    {
+        throw std::invalid_argument("invalid completed migration buffer");
+    }
+    std::size_t offset{0};
+    sharpen::Varuint64 builder{0};
+    offset += Helper::LoadFrom(builder,data,size);
+    this->id_ = builder.Get();
+    if(size < offset + 4)
+    {
+        throw sharpen::DataCorruptionException("completed migration corruption");
+    }
+    offset += Helper::LoadFrom(builder,data + offset,size - offset);
+    this->source_ = builder.Get();
+    if(size < offset + 3)
+    {
+        throw sharpen::DataCorruptionException("completed migration corruption");
+    }
+    offset += Helper::LoadFrom(builder,data + offset,size - offset);
+    this->destination_ = builder.Get();
+    if (size < offset + 2)
+    {
+        throw sharpen::DataCorruptionException("completed migration corruption");
+    }
+    offset += Helper::LoadFrom(this->beginKey_,data + offset,size - offset);
+    if (size == offset)
+    {
+        throw sharpen::DataCorruptionException("completed migration corruption");
+    }
+    offset += Helper::LoadFrom(this->endKey_,data + offset,size - offset);
+    return offset;
+}
+
+std::size_t rkv::CompletedMigration::UnsafeStoreTo(char *data) const noexcept
+{
+    std::size_t offset{0};
+    sharpen::Varuint64 builder{this->id_};
+    offset += Helper::UnsafeStoreTo(builder,data);
+    builder.Set(this->source_);
+    offset += Helper::UnsafeStoreTo(builder,data + offset);
+    builder.Set(this->destination_);
+    offset += Helper::UnsafeStoreTo(builder,data + offset);
+    offset += Helper::UnsafeStoreTo(this->beginKey_,data + offset);
+    offset += Helper::UnsafeStoreTo(this->endKey_,data + offset);
+    return offset;
+}
